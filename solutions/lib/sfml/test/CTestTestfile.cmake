3df6880687edcf9d8d586b28cb8bb7a9954af10e
SweetPrince747 <40534169@live.napier.ac.uk>
2024-10-03 09:09:17 +0100

    add folder for cmake solutions

---
+++ solutions/lib/sfml/test/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test
+# Build directory: C:/Games_Eng_Builds/lib/sfml/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test-sfml-system "test-sfml-system")
+set_tests_properties(test-sfml-system PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/danfe/source/repos/Games-Eng/lib/sfml/cmake/Macros.cmake;309;add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;15;sfml_add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;0;")
+add_test(test-sfml-window "test-sfml-window")
+set_tests_properties(test-sfml-window PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/danfe/source/repos/Games-Eng/lib/sfml/cmake/Macros.cmake;309;add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;23;sfml_add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;0;")
+add_test(test-sfml-graphics "test-sfml-graphics")
+set_tests_properties(test-sfml-graphics PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/danfe/source/repos/Games-Eng/lib/sfml/cmake/Macros.cmake;309;add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;33;sfml_add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;0;")
+add_test(test-sfml-network "test-sfml-network")
+set_tests_properties(test-sfml-network PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/danfe/source/repos/Games-Eng/lib/sfml/cmake/Macros.cmake;309;add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;41;sfml_add_test;C:/Users/danfe/source/repos/Games-Eng/lib/sfml/test/CMakeLists.txt;0;")
